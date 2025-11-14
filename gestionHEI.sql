1872ff412c58eec2d0187a543fd662a2bfa1c1e2
mekill404 <hei.mahery.55@gmail.com>
2025-11-14 22:07:01 +0300

    Fix formatting of query to display employees who have never taken leave in gestionHEI.sql

--- gestionHEI.sql
+++ gestionHEI.sql
@@ -51,10 +51,10 @@ FROM Employe
 WHERE id_team IS NULL;
 
 --Afficher l’id, first_name, last_name des employés qui n’ont jamais pris de congé de leur vie.
--- SELECT e.id, e.first_name, e.last_name
--- FROM Employe AS e
--- LEFT JOIN Leave AS l ON e.id = l.id_employe
--- WHERE l.id IS NULL;
+SELECT e.id, e.first_name, e.last_name
+FROM Employe AS e
+LEFT JOIN Leave AS l ON e.id = l.id_employe
+WHERE l.id IS NULL;
 --Afficher les congés de tel sorte qu’on voie l’id du congé, le début du congé, la fin du congé, le nom & prénom de l’employé qui prend congé et le nom de son équipe.
 --Affichez par le nombre d’employés par contract_type, vous devez afficher le type de contrat, et le nombre d’employés associés.
 --Afficher le nombre d’employés en congé aujourd'hui. La période de congé s'étend de start_date inclus jusqu’à end_date inclus.
