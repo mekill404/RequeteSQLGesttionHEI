1d7ed04612cdfbbd839c39487c83029613f395dd
mekill404 <hei.mahery.55@gmail.com>
2025-11-14 23:01:55 +0300

    Fix

--- gestionHEI.sql
+++ gestionHEI.sql
@@ -15,7 +15,11 @@ CREATE TABLE Team
  name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO Team VALUES (DEFAULT, 'AXIAN'), (DEFAULT, 'SODIATE'), (DEFAULT,'TGV');
+INSERT INTO Team VALUES (DEFAULT, 'AXIAN'),
+  (DEFAULT, 'SODIATE'), 
+  (DEFAULT,'TGV'),
+  (DEFAULT,'HEI')
+  (DEFAULT,'MADA');
 
 CREATE TABLE Employe
 (
@@ -42,7 +46,10 @@ CREATE TABLE Leave
   id_employe INTEGER REFERENCES Employe(id)
 );
 
-INSERT INTO Leave VALUES (DEFAULT, '2025-11-14', '2025-11-20', 1), (DEFAULT, '2025-11-30', '2026-01-05', 3);
+INSERT INTO Leave VALUES (DEFAULT, '2025-11-14', '2025-11-20', 1), 
+  (DEFAULT, '2025-11-30', '2026-01-05', 3),
+  (DEFAULT, '2025-12-15', '2025-12-25', 4),
+  (DEFAULT, '2025-11-10', '2025-11-12', 1);
 
 
 --Afficher l’id, first_name, last_name des employés qui n’ont pas d’équipe.
@@ -56,7 +63,20 @@ FROM Employe AS e
 LEFT JOIN Leave AS l ON e.id = l.id_employe
 WHERE l.id IS NULL;
 --Afficher les congés de tel sorte qu’on voie l’id du congé, le début du congé, la fin du congé, le nom & prénom de l’employé qui prend congé et le nom de son équipe.
---Affichez par le nombre d’employés par contract_type, vous devez afficher le type de contrat, et le nombre d’employés associés.
+SELECT l.id, l.start_date, l.end_date, e.first_name, e.last_name
+FROM Employe AS e
+INNER JOIN Leave AS l ON l.id_employe = e.id;
+--Affichez le nombre d’employés par type  contract_type, vous devez afficher le type de contrat, et le nombre d’employés associés.
+SELECT contract_type, COUNT(*) AS number_of_employees
+FROM Employe
+GROUP BY contract_type;
 --Afficher le nombre d’employés en congé aujourd'hui. La période de congé s'étend de start_date inclus jusqu’à end_date inclus.
+SELECT COUNT(*) AS number_of_employees_on_leave_today
+FROM Leave
+WHERE CURRENT_DATE BETWEEN start_date AND end_date;
 --Afficher l’id, le nom, le prénom de tous les employés + le nom de leur équipe qui sont en congé aujourd’hui. Pour rappel, la end_date est incluse dans le congé, l’employé ne revient que le lendemain. 
-
+SELECT e.id, e.first_name, e.last_name, t.name AS team_name
+FROM Employe AS e
+LEFT JOIN Team AS t ON e.id_team = t.id
+INNER JOIN Leave AS l ON e.id = l.id_employe
+WHERE CURRENT_DATE BETWEEN l.start_date AND l.end_date;
